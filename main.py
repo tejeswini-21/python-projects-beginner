eea45675b50026f471f8449b8b52a7c96f723fdd
tejeswini-21 <siddutejeswini@gmail.com>
2026-06-16 23:16:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-#Weight Converter
-weight=float(input("Enter the weight: "))
-unit=input("Kilograms Or Pounds(K / L): ").upper()
-if unit == "K":
-    weight = weight * 2.205
-    unit ="Lbs."
-    print(f"Your weight is {round(weight, 1)} {unit}")
-elif unit == "L":
-    weight = weight / 2.205
-    unit = "Kgs."
-    print(f"Your weight is {round(weight, 1)} {unit}")
-else:
-    print(f"{unit} is not a valid")
+# Compound Interest Calculator
+principal = 0
+rate = 0
+time = 0
+
+while principal <= 0:
+    principal = float(input("Enter the Principal amount: ").replace(",", ""))
+    if principal <= 0:
+        print("Principal amount must be greater than zero.")
+
+while rate <= 0:
+    rate = float(input("Enter the annual interest rate (%): "))
+    if rate <= 0:
+        print("Interest rate must be greater than zero.")
+
+while time <= 0:
+    time = int(input("Enter Time in years: "))
+    if time <= 0:
+        print("Time period must be greater than zero.")
+total = principal * pow ((1+rate/100), time)
+print(f"Your total amount after {time} years is ₹{total:,.2f}")
